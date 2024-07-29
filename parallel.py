d0fcfa8d0474cb5c0e6c46afac48928b37badeb4
kongdoll_bamdoll <kongdoll_bamdoll@example.com>
2024-07-29 16:17:08 +0900

    again_check

--- parallel.py
+++ parallel.py
@@ -38,6 +38,23 @@ def calculate_efficiencies(K, all_riders, all_orders, dist_mat):
     # 효율성 지표 반환
     return efficiencies
 
+# def find_nearest_orders(current_bundle, remaining_orders, dist_mat, K, num_orders=30):
+#     bundle_size = len(current_bundle)
+    
+#     distances = []
+#     for order in remaining_orders:
+#         total_distance = 0
+#         for existing_order in current_bundle:
+#             pickup_distance = dist_mat[existing_order.id, order.id]
+#             delivery_distance = dist_mat[existing_order.id + K, order.id + K]
+#             total_distance += (pickup_distance + delivery_distance)
+#         average_distance = total_distance / bundle_size
+#         distances.append((order, average_distance))
+    
+#     distances.sort(key=lambda x: x[1])
+#     nearest_orders = [(order, avg_dist) for order, avg_dist in distances[:num_orders]]
+#     return nearest_orders
+
 def find_nearest_orders(current_bundle, remaining_orders, dist_mat, K, num_orders=30):
     bundle_size = len(current_bundle)
     
@@ -54,8 +71,6 @@ def find_nearest_orders(current_bundle, remaining_orders, dist_mat, K, num_order
     distances.sort(key=lambda x: x[1])
     nearest_orders = [order for order, _ in distances[:num_orders]]
     return nearest_orders
-
-
 
 def assign_orders_to_rider(rider, orders, dist_mat, K, all_orders):
     bundles = []
@@ -79,7 +94,7 @@ def assign_orders_to_rider(rider, orders, dist_mat, K, all_orders):
             nearest_orders = find_nearest_orders(current_bundle, remaining_orders, dist_mat, K, 30)
             added = False
 
-            if K > 200 and len(current_bundle) >= 4:
+            if len(current_bundle) >= 4:
                 break
             
             for next_order in nearest_orders:
@@ -129,6 +144,98 @@ def assign_orders_to_rider(rider, orders, dist_mat, K, all_orders):
 
     return bundles, remaining_orders
 
+
+# def assign_orders_to_rider(rider, orders, dist_mat, K, all_orders):
+#     bundles = []
+#     remaining_orders = orders[:]
+    
+#     while remaining_orders and rider.available_number > 0:
+#         current_order = remaining_orders.pop(0)
+#         current_bundle = [current_order]
+#         shop_seq = [current_order.id]
+#         delivery_seq = sorted(shop_seq, key=lambda order_id: all_orders[order_id].deadline)
+        
+#         is_feasible = test_route_feasibility(all_orders, rider, shop_seq, delivery_seq)
+#         if is_feasible != 0:
+#             remaining_orders.insert(0, current_order)
+#             return bundles, remaining_orders
+        
+#         current_volume = current_order.volume
+#         current_time = current_order.ready_time
+
+#         while True:
+#             nearest_orders_with_dist = find_nearest_orders(current_bundle, remaining_orders, dist_mat, K, 30)
+            
+#             # 거리 가중치 계산
+#             distance_weights = np.array([1 / (avg_dist + 1) for _, avg_dist in nearest_orders_with_dist])
+            
+#             # nearest_orders 리스트 추출
+#             nearest_orders = [order for order, _ in nearest_orders_with_dist]
+
+#             # nearest_orders를 배달 마감시간 기준으로 정렬
+#             nearest_orders.sort(key=lambda order: order.deadline)
+            
+#             # 배달 마감시간 가중치 계산
+#             deadline_weights = np.array([1 / (order.deadline + 1) for order in nearest_orders])
+            
+#             # 최종 가중치 계산
+#             combined_weights = deadline_weights + distance_weights
+#             probabilities = combined_weights / combined_weights.sum()
+
+#             added = False
+
+#             if len(current_bundle) >= 4:
+#                 break
+            
+#             # 확률에 따라 next_order 선택
+#             next_order = np.random.choice(nearest_orders, p=probabilities)
+
+#             if current_volume + next_order.volume > rider.capa:
+#                 continue
+
+#             current_bundle_ids = [o.id for o in current_bundle]
+#             next_bundle_ids = [next_order.id]
+
+#             combined_ids = current_bundle_ids + next_bundle_ids
+#             pickup_permutations = itertools.permutations(combined_ids)
+
+#             valid_combinations = []
+            
+#             for perm_shop_seq in pickup_permutations:
+#                 delivery_permutations = itertools.permutations(perm_shop_seq)
+#                 for perm_dlv_seq in delivery_permutations:
+#                     new_bundle = Bundle(all_orders, rider, list(perm_shop_seq), list(perm_dlv_seq), current_volume + next_order.volume, 0)
+#                     new_bundle.total_dist = get_total_distance(K, dist_mat, list(perm_shop_seq), list(perm_dlv_seq))
+#                     new_bundle.update_cost()
+
+#                     is_feasible = test_route_feasibility(all_orders, rider, list(perm_shop_seq), list(perm_dlv_seq))
+#                     if is_feasible == 0:
+#                         valid_combinations.append((list(perm_shop_seq), list(perm_dlv_seq)))
+
+#             if valid_combinations:
+#                 best_combination = min(valid_combinations, key=lambda x: get_total_distance(K, dist_mat, x[0], x[1]))
+#                 best_shop_seq, best_dlv_seq = best_combination
+
+#                 current_bundle.append(next_order)
+#                 current_volume += next_order.volume
+#                 current_time += rider.T[current_bundle[-2].id, next_order.id]
+#                 remaining_orders.remove(next_order)
+#                 added = True
+
+#                 # 선택된 best_shop_seq와 best_dlv_seq로 번들 갱신
+#                 shop_seq = best_shop_seq
+#                 delivery_seq = best_dlv_seq
+
+#             if not added:
+#                 break
+
+#         final_bundle = Bundle(all_orders, rider, shop_seq, delivery_seq, current_volume, get_total_distance(K, dist_mat, shop_seq, delivery_seq))
+#         bundles.append(final_bundle)
+#         rider.available_number -= 1
+
+#     return bundles, remaining_orders
+
+
 def assign_riders_with_weighted_probability(riders, effectiveness_indicator):
     total_effectiveness = sum([indicator[1] for indicator in effectiveness_indicator])
     weights = [total_effectiveness / indicator[1] for indicator in effectiveness_indicator]
@@ -142,7 +249,8 @@ def single_run_algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
         r.T = np.round(dist_mat / r.speed + r.service_time)
 
     # 효율성 지표 (예시로 임의의 값 사용)
-    effectiveness_indicator = [['bike', 100], ['car', 200], ['walk', 300]]
+    #effectiveness_indicator = [['bike', 100], ['car', 200], ['walk', 300]]
+    effectiveness_indicator = calculate_efficiencies(K, all_riders, all_orders, dist_mat)
     effectiveness_dict = {rider.type: effectiveness for rider, effectiveness in zip(all_riders, effectiveness_indicator)}
 
     # 주문들을 ready_time 기준으로 정렬
@@ -163,52 +271,6 @@ def single_run_algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
     best_obj = sum((bundle.cost for bundle in all_bundles)) / K
     print(f'Initial best obj = {best_obj}')
 
-    # while time.time() - start_time < timelimit:
-    #     iter = 0
-    #     max_merge_iter = 1000
-
-    #     while iter < max_merge_iter and time.time() - start_time < timelimit:
-    #         bundle1, bundle2 = select_two_bundles(all_bundles)
-    #         new_bundle = try_merging_bundles(K, dist_mat, all_orders, bundle1, bundle2)
-    #         if new_bundle is not None:
-    #             is_feasible = test_route_feasibility(all_orders, new_bundle.rider, new_bundle.shop_seq, new_bundle.dlv_seq)
-    #             if is_feasible == 0:
-    #                 all_bundles.remove(bundle1)
-    #                 bundle1.rider.available_number += 1
-
-    #                 all_bundles.remove(bundle2)
-    #                 bundle2.rider.available_number += 1
-
-    #                 all_bundles.append(new_bundle)
-    #                 new_bundle.rider.available_number -= 1
-
-    #                 cur_obj = sum((bundle.cost for bundle in all_bundles)) / K
-    #                 if cur_obj < best_obj:
-    #                     best_obj = cur_obj
-    #                     print(f'New best obj after merge = {best_obj}')
-    #         else:
-    #             iter += 1
-
-    #     for bundle in all_bundles:
-    #         new_rider = get_cheaper_available_riders(all_riders, bundle.rider)
-    #         if new_rider is not None and new_rider.available_number > 0:
-    #             old_rider = bundle.rider
-    #             temp_bundle = bundle
-    #             if try_bundle_rider_changing(all_orders, dist_mat, temp_bundle, new_rider):
-    #                 is_feasible = test_route_feasibility(all_orders, new_rider, temp_bundle.shop_seq, temp_bundle.dlv_seq)
-    #                 if is_feasible == 0:
-    #                     old_rider.available_number += 1
-    #                     new_rider.available_number -= 1
-    #                     bundle.rider = new_rider
-    #                     bundle.shop_seq = temp_bundle.shop_seq
-    #                     bundle.dlv_seq = temp_bundle.dlv_seq
-    #                     bundle.update_cost()
-
-    #     cur_obj = sum((bundle.cost for bundle in all_bundles)) / K
-    #     if cur_obj < best_obj:
-    #         best_obj = cur_obj
-    #         print(f'New best obj after rider reassignment = {best_obj}')
-
     solution = [
         [bundle.rider.type, bundle.shop_seq, bundle.dlv_seq]
         for bundle in all_bundles
@@ -216,7 +278,7 @@ def single_run_algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
 
     return solution, best_obj
 
-def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60, num_processes=10):
+def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60, num_processes=30):
     with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as executor:
         futures = [executor.submit(single_run_algorithm, K, all_orders, all_riders, dist_mat, timelimit) for _ in range(num_processes)]
         results = [future.result() for future in concurrent.futures.as_completed(futures)]
